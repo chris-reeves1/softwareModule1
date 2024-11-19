375197224095dcd7d0f441edb93e250257370ee2
chris-reeves1 <dr_gonzo19@hotmail.com>
2024-11-19 10:28:54 +0000

    new commit

---
+++ python/lessons/conditionals.py
@@ -0,0 +1,76 @@
+# if, elif, else
+# different pathways for our code to follow. 
+
+
+#my_bool = False
+
+#if my_bool:
+#    print("mybool is true")
+#else:
+#    print("my_bool must be false!")
+
+
+#if some condition:
+#    if come other condtion:
+#        code.....
+#    else:
+#        code ....
+#else: 
+#    code ....
+
+#is_admin = True
+#is_verified = False
+#on_holiday = False
+
+#if (is_admin or is_verified) and not on_holiday:
+#    print("access granted")
+#else:
+#    print("access denied")
+
+# == equal to
+# > greeater than
+# < less than
+# >= greatre than or equals to
+# <= less than orequals to
+# != not equals to. 
+
+temp = 30
+
+#if temp >= 30:
+#    print("its hot")
+#if temp > 25 and temp < 30:
+#    print("pretty hot")
+#if temp > 10 and temp < 25:
+#    print("its ok")
+#if temp == 0:
+#    print("very cold")
+#else:
+#    print("anything below 10 degrees")
+
+#elif == else if
+
+if temp >= 30:
+    print("its hot")
+elif temp > 25:
+    print("pretty hot")
+elif temp > 10:
+    print("its ok")
+elif temp == 0:
+    print("very cold")
+else:
+    print("anything below 10 degrees")
+
+# exercise:
+
+# user to input grade/mark
+# if the mark is above 85 print distiction
+# between 65 and 85 print pass
+# anything else if fail. 
+# use if elif else. 
+
+
+
+
+
+
+
