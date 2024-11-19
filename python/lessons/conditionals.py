9867bfc102e901b9b5e942ca24ad4e4bd88efe01
chris-reeves1 <dr_gonzo19@hotmail.com>
2024-11-19 10:59:07 +0000

    new commit

--- python/lessons/conditionals.py
+++ python/lessons/conditionals.py
@@ -49,16 +49,16 @@ temp = 30
 
 #elif == else if
 
-if temp >= 30:
-    print("its hot")
-elif temp > 25:
-    print("pretty hot")
-elif temp > 10:
-    print("its ok")
-elif temp == 0:
-    print("very cold")
-else:
-    print("anything below 10 degrees")
+#if temp >= 30:
+#    print("its hot")
+#elif temp > 25:
+#    print("pretty hot")
+#elif temp > 10:
+#    print("its ok")
+#elif temp == 0:
+#    print("very cold")
+#else:
+#    print("anything below 10 degrees")
 
 # exercise:
 
@@ -68,7 +68,54 @@ else:
 # anything else if fail. 
 # use if elif else. 
 
+#x = int(input("enter a grade: "))
 
+#if x >= 85:
+#    print("distiction")
+#elif x >= 65:
+#    print("pass")
+#else:
+#    print("fail")
+
+# multiple comparators
+
+#deposit = 199
+#password = "password1"
+ 
+#if 0 < deposit < 100 and password == "password10":
+#    print(f"thankyou for deposit of {deposit}")
+#else:
+#    print("deposit failed")
+
+# not
+
+#if not 0 < deposit < 100 and password != "password":
+#    print("deposit failed")
+#else:
+#    print("Thanks for deposit")
+
+# in and not in
+
+#name = "root123"
+
+#if name in ("root", "admin", "user"):
+#    print("invalid username")
+#else:
+    #print("accepted")
+
+#if name not in ("root", "admin", "user"):
+#    print("accepted")
+#else:
+#    print("invalid username")
+
+# exercise
+# weight converter
+# user to input weight
+# user to input kgs or lbs
+# if stement to check for unit entered
+# logic to convert the wight and print out the converted value. 
+# error handling for upper/lowercase
+# optional - error handling for input validation. 
 
 
 
