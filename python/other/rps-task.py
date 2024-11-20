3a57daee7ed47b3d410a60727ff1352905ede99a
chris-reeves1 <dr_gonzo19@hotmail.com>
2024-11-20 13:36:13 +0000

    new commit

---
+++ python/other/rps-task.py
@@ -0,0 +1,5 @@
+# Group exercise
+# Rock paper scissor game i can play against the computer.
+# the app should keep track of rounds played, user wins, computer wins. 
+# Use a function for game logic and a seperate while loop 
+# for menu/printing/play-again option. 
